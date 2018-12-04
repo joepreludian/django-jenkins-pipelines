462b6d5b641984f57a89fc8d86910fce1d23a70c
Jonhnatha Trigueiro <joepreludian@gmail.com>
2018-12-04 02:47:14 -0200

    Update base pipeline

--- django_pipelines.groovy
+++ django_pipelines.groovy
@@ -69,127 +69,140 @@ def djangoBuildProject(args) {
             }
         }
     
-        // Build Javascript stuff, if needed
-        if (args.node_install_static_npm_modules) {
-            docker.image("node").inside(args.docker_extra_options) {
-                unstash 'django_static'
+        stage('Build Node stuff') {
+            // Build Javascript stuff, if needed
+            if (args.node_install_static_npm_modules) {
+                docker.image("node").inside(args.docker_extra_options) {
+                    unstash 'django_static'
 
-                sh 'npm install -g yarn'
-                sh 'cd static; yarn'
+                    sh 'npm install -g yarn'
+                    sh 'cd static; yarn'
 
-                stash includes: 'static/', name: 'django_static_final'
-            }
-        }
-
-    }
-}
-
-pipeline {
-
-    agent none
-
-    options {
-        buildDiscarder(
-            logRotator(
-                numToKeepStr: '4', 
-                artifactNumToKeepStr: '4'
-            )
-        )
-    }
-
-    triggers {
-        pollSCM('H/30 * * * *')
-    }
-
-    environment {
-        DJANGO_PRODUCTION = '1'
-    }
-
-    stages {
-
-        stage('Build Django') {
-            agent any
-            steps {
-                projectPipelineStart docker_extra_options: docker_args, wsgi: 'test_project', main_module: 'Main Module'
-            }
-        }
-
-        stage('Build project - Install JS Libraries') {
-            agent {
-                docker { 
-                    image 'node'
-                    args docker_args
+                    stash includes: 'static/', name: 'django_static_final'
                 }
             }
-            steps {
-                figlet 'JS - Install Libraries'
-                unstash 'django_static'
-
-                sh 'npm install -g yarn'
-                sh 'cd static; yarn'
-
-                stash includes: 'static/', name: 'django_static_final'
-            }
-        }
-
-        stage('Generate Artifact') {
-            agent any
-            steps {
-                cleanWs()
-                checkout scm
-
-                unstash 'django_static_final'
-
-                figlet 'Django - Generate artifact'
-                zip zipFile: 'project.zip', dir: '.'
-
-                stash includes: 'project.zip', name: 'django_project'
-                
-            }
-        }
-
-        stage ('Create container') {
-            agent any
-            steps {
-                cleanWs()
-                
-                unstash 'django_project'
-                
-                echo 'Deployment container'
-                                    
-                sh 'wget https://github.com/joepreludian/django-jenkins-pipelines/archive/master.zip -O pipeline_temp.zip'
-                sh 'unzip pipeline_temp.zip -d .'
-
-                sh "mkdir dist; cd dist; unzip ../project.zip -d .; cp -Rv ../django-jenkins-pipelines-master/django_jenkins_pipeline/* ."
-                
-                sh "cd dist; cat supervisord.conf"
-                sh "cd dist; sed -i \'s/%python_version%/${project_python_version}/g\' Dockerfile"
-                sh "cd dist; sed -i \'s/%project_name%/${wsgi_container}/g\' supervisord.conf"
-
-                zip zipFile: project_zip, dir: 'dist'
-                archiveArtifacts artifacts: project_zip, fingerprint: true
-
-                sh "cd dist; docker build -t ${docker_image_name}:${project_version} -t ${docker_image_name}:latest ."  // @TODO Refactor docker image to test the latest 
-            }
-        }
-
-        stage('Deploy docker container') {
-            agent any
-            steps {
-
-                echo "Using Docker image: ${docker_image_name}:${project_version}"
-                echo "Removing running instances"
-            
-                // @TODO Refactor this to handle it efficiently
-
-                sh "docker stop ${project_name} || true"
-                sh "docker rm ${project_name} || true"
-
-                sh "docker run -it -d --name ${project_name} -p ${docker_listen_port}:80 ${docker_image_name}:${project_version}"
-
-            }
         }
 
     }
 }
 
+/*
+def runPipeline(params) {
+    pipeline {
+
+        agent none
+
+        options {
+            buildDiscarder(
+                logRotator(
+                    numToKeepStr: '4', 
+                    artifactNumToKeepStr: '4'
+                )
+            )
+        }
+
+        triggers {
+            pollSCM('H/30 * * * *')
+        }
+
+        environment {
+            DJANGO_PRODUCTION = '1'
+        }
+
+        stages {
+
+            stage('Build Django') {
+                agent any
+                steps {
+                    projectPipelineStart docker_extra_options: docker_args, wsgi: 'test_project', main_module: 'Main Module'
+                }
+            }
+
+            stage('Build project - Install JS Libraries') {
+                agent {
+                    docker { 
+                        image 'node'
+                        args docker_args
+                    }
+                }
+                steps {
+                    figlet 'JS - Install Libraries'
+                    unstash 'django_static'
+
+                    sh 'npm install -g yarn'
+                    sh 'cd static; yarn'
+
+                    stash includes: 'static/', name: 'django_static_final'
+                }
+            }
+
+            stage('Generate Artifact') {
+                agent any
+                steps {
+                    cleanWs()
+                    checkout scm
+
+                    unstash 'django_static_final'
+
+                    figlet 'Django - Generate artifact'
+                    zip zipFile: 'project.zip', dir: '.'
+
+                    stash includes: 'project.zip', name: 'django_project'
+                    
+                }
+            }
+
+            stage ('Create container') {
+                agent any
+                when {
+                    branch 'master'
+                }
+                steps {
+                    cleanWs()
+                    
+                    unstash 'django_project'
+                    
+                    echo 'Deployment container'
+                                        
+                    sh 'wget https://github.com/joepreludian/django-jenkins-pipelines/archive/master.zip -O pipeline_temp.zip'
+                    sh 'unzip pipeline_temp.zip -d .'
+
+                    sh "mkdir dist; cd dist; unzip ../project.zip -d .; cp -Rv ../django-jenkins-pipelines-master/django_jenkins_pipeline/* ."
+                    
+                    sh "cd dist; cat supervisord.conf"
+                    sh "cd dist; sed -i \'s/%python_version%/${project_python_version}/g\' Dockerfile"
+                    sh "cd dist; sed -i \'s/%project_name%/${wsgi_container}/g\' supervisord.conf"
+
+                    zip zipFile: project_zip, dir: 'dist'
+                    archiveArtifacts artifacts: project_zip, fingerprint: true
+
+                    sh "cd dist; docker build -t ${docker_image_name}:${project_version} -t ${docker_image_name}:latest ."  // @TODO Refactor docker image to test the latest 
+                }
+            }
+
+            stage('Deploy docker container') {
+                agent any
+                when {
+                    branch 'master'
+                }
+                steps {
+
+                    echo "Using Docker image: ${docker_image_name}:${project_version}"
+                    echo "Removing running instances"
+                
+                    // @TODO Refactor this to handle it efficiently
+
+                    params['buildClosure']()
+
+                    sh "docker stop ${project_name} || true"
+                    sh "docker rm ${project_name} || true"
+
+                    sh "docker run -it -d --name ${project_name} -p ${docker_listen_port}:80 ${docker_image_name}:${project_version}"
+
+                }
+            }
+
+        }
+    }
+}
+*/
