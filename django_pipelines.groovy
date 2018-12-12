24807255ba1110612e9a206f78d9da297e97830b
Jonhnatha Trigueiro <joepreludian@gmail.com>
2018-12-12 00:31:28 -0200

    Update Jenkinsfile

--- django_pipelines.groovy
+++ django_pipelines.groovy
@@ -43,14 +43,15 @@ def buildProject(args) {
       if (!docker_config_jenkins_home_vol)
         error "Jenkins configuration not found - please set docker_config_jenkins_home_vol or DJANGO_PIPELINES_JENKINS_HOME_VOL"
 
-      def docker_extra_options = args.docker_extra_options ? args.docker_extra_options : "-v ${docker_config_jenkins_home_vol}:/var/jenkins_home -u root:root"
-      docker_extra_params = "${docker_extra_options} --link ${db_container.id}:${args.mysql_sidecar.host_name}"
-      echo "-- docker_extra_params: ${docker_extra_params}"
-
+        
       docker.image("mysql:${args.mysql_sidecar.version}").withRun("-e \"MYSQL_ROOT_PASSWORD=${args.mysql_sidecar.root_password}\" -e \"MYSQL_DATABASE=${args.mysql_sidecar.database_name}\"") { db_container ->
-
+        
         sh 'echo "Waiting Mysql Being ready..." && sleep 4'
         
+        def docker_extra_options = args.docker_extra_options ? args.docker_extra_options : "-v ${docker_config_jenkins_home_vol}:/var/jenkins_home -u root:root"
+        docker_extra_params = "${docker_extra_options} --link ${db_container.id}:${args.mysql_sidecar.host_name}"
+        echo "-- docker_extra_params: ${docker_extra_params}"
+
         docker.image("python:${args.python_version}").inside(docker_extra_params) {
             stage('Install dependencies') {
                 figlet "${args.project_name} - ${project_version}"
